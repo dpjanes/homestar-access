d47e314ca7b292939e6994453193cdcdd50a77e5
David Janes <davidjanes@davidjanes.com>
2015-05-06 17:32:08 -0400

    first release

--- DIST.sh
+++ DIST.sh
@@ -6,7 +6,7 @@
 #   2015-02-10
 #
 
-PACKAGE=homestar-THEPACKAGE
+PACKAGE=homestar-access
 DIST_ROOT=/var/tmp/.dist.$$
 
 if [ ! -d "$DIST_ROOT" ]
@@ -33,8 +33,8 @@ echo "=================="
         --exclude "node_modules" \
         README.md LICENSE \
         homestar.json package.json \
-        BRIDGE.js index.js \
-        models/*js \
+        index.js access.js \
+        dynamic/*.html \
         |
     ( cd "${NPM_DST}" && tar xvf - )
 
@@ -43,3 +43,5 @@ echo "=================="
 
     echo "end"
 )
+
+
--- README.md
+++ README.md
@@ -4,4 +4,14 @@ HomeStar Access Control Module
 <img src="https://github.com/dpjanes/iotdb-homestar/blob/master/docs/HomeStar.png" align="right" />
 
 This module allows you to specify who can use your 
-various Things and Recipes.
+various Things and Cookbooks.
+
+# Installation
+
+Install Home☆Star first. 
+See: https://github.com/dpjanes/iotdb-homestar#installation
+
+Then
+
+    $ homestar install homestar-access
+
